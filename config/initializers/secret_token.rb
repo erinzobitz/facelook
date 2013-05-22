06ba214c431cdaf2049aa52e699be6fe8a9708ae
Erin Zobitz <erinzobitz@gmail.com>
2013-05-21 22:35:09 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facelook::Application.config.secret_token = 'fc76aa64acb1a339aec211b3a86313aeae8eab638a99bedd46dc833c8d0cf7b7d8525a717b0cd106b6f8f734ca592d65e8ae27accf4e680ac80ccb7eb97e4263'
